f8224d608d39bde051fa1c22b708eafbd76a9467
Alejopizarro <alejomkt10@gmail.com>
2024-12-10 20:46:47 +0100

    Revert "Configuracion de controller, service y repository"
    This reverts commit 0d7c535be05b9a67d2a2492453e82292d618ce15.

--- src/main/java/com/example/games_services_api/repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.games_services_api.repository;
-
-import com.example.games_services_api.commons.entities.GameModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<GameModel, Long> {
-}
--- src/main/java/com/example/games_services_api/service/GameService.java
+++
@@ -1,7 +0,0 @@
-package com.example.games_services_api.service;
-
-import com.example.games_services_api.commons.entities.GameModel;
-
-public interface GameService {
-    GameModel createGame(GameModel gameRequest);
-}
